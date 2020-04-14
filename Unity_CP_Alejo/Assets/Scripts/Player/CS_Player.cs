24e4e27594b4ce9499c9a27d0c23859f0e074ce9
KevinGmGr <kevingmgr@gmail.com>
2020-04-14 21:04:57 +0200

    TODOs added

--- Unity_CP_Alejo/Assets/Scripts/Player/CS_Player.cs
+++ Unity_CP_Alejo/Assets/Scripts/Player/CS_Player.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class CS_Player : MonoBehaviour
 {
     CharacterController CCRef;
-    float Speed = 10; 
+
+    [SerializeField]
+    float Speed = 10;
+    [SerializeField] //SerializeField makes variables editable from Unity Editor
     float JumpForce = 10;
     Vector3 Movement = Vector3.zero;
 
-    
+    //TODO: create variable for Horizontal rotation speed named RotHorizontalSpeed
+    //TODO: create variable for Vertical rotation speed named RotVerticalSpeed
+
     void Start()
     {
         CCRef = this.gameObject.GetComponent<CharacterController>();  
@@ -17,26 +22,38 @@ public class CS_Player : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {        
-        if(CCRef.isGrounded)
+    {
+        //TODO: Add rotation on Y axis by using transform.Rotate() and reading Input.GetAxis("Mouse X") and multiply by RotHorizontalSpeed
+        //TODO: Add rotation on X axis to gameObject Camera by using transform.Rotate() and reading Input.GetAxis("Mouse Y") and multiply by RotVerticalSpeed
+        if (CCRef.isGrounded)
         {
+            //Refactor: Place this code inside of function LandedMovement ()
+            //->
+            //Movement = LandedMovement()
             Movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
             Movement = this.gameObject.transform.TransformDirection(Movement);
             Movement *= Speed;
+            //-<
 
             if(Input.GetButton("Jump"))
             {
+                //Place this code inside of function Jump ()
+                //->
                 Movement.y += JumpForce;
+                //-<
             }
         }
         else
         {
             Movement += Physics.gravity * Time.deltaTime;
-        }
+        }        
 
         
-
-        CCRef.Move(Movement * Time.deltaTime);
-        
+        CCRef.Move(Movement * Time.deltaTime);        
     }
+
+    //TODO: Create function for Landed Movement -> Vector3 LandedMovement ()
+
+    //TODO: Create function of Jump 
+
 }
