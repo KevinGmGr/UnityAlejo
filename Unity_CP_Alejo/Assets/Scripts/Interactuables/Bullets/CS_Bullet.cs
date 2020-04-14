b59e443f67c6b07a386f96380199e93da71da70d
KevinGmGr <kevingmgr@gmail.com>
2020-04-14 20:30:12 +0200

    MainCommit

---
+++ Unity_CP_Alejo/Assets/Scripts/Interactuables/Bullets/CS_Bullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_Bullet : MonoBehaviour
+{
+    public float Speed = 1;
+    public float Damage = 10.0f;
+    //TODO: Guardar una referencia quien crea la bala
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+        this.gameObject.transform.position += this.gameObject.transform.TransformDirection (Vector3.forward * Speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("HSAFA");
+    }
+}
---
+++ Unity_CP_Alejo/Assets/Scripts/Player/CS_Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_Player : MonoBehaviour
+{
+    CharacterController CCRef;
+    float Speed = 10; 
+    float JumpForce = 10;
+    Vector3 Movement = Vector3.zero;
+
+    
+    void Start()
+    {
+        CCRef = this.gameObject.GetComponent<CharacterController>();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        if(CCRef.isGrounded)
+        {
+            Movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            Movement = this.gameObject.transform.TransformDirection(Movement);
+            Movement *= Speed;
+
+            if(Input.GetButton("Jump"))
+            {
+                Movement.y += JumpForce;
+            }
+        }
+        else
+        {
+            Movement += Physics.gravity * Time.deltaTime;
+        }
+
+        
+
+        CCRef.Move(Movement * Time.deltaTime);
+        
+    }
+}
